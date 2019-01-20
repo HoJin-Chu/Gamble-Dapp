03140f1f838123ec877456cd66b98e118ae5a3ad
hojin-chu <cnghwls12@naver.com>
2019-01-21 01:07:58 +0900

    dispatch

--- src/components/CasinoComponent.vue
+++ src/components/CasinoComponent.vue
@@ -30,6 +30,11 @@
 
 <script>
 export default {
+    name: 'casino',
+    mounted () {
+    console.log('dispatching getContractInstance')
+    this.$store.dispatch('getContractInstance')
+    },
     data () {
     return {
         amount: null,
